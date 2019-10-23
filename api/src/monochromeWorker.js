0310e9b78f0ebaf6fc0816d5e07f482cf34b7d62
AuggieH <hudakjerome@gmail.com>
2019-10-22 21:43:20 -0400

    reset code to initial state

--- api/src/monochromeWorker.js
+++
@@ -1,9 +0,0 @@
-const gm = require('gm');
-const {workerData, parentPort} = require('worker_threads');
-
-gm(workerData.source)
-    .monochrome()
-    .write(workerData.destination, (error) => {
-      if ( error ) throw error;
-      parentPort.postMessage({monochrome: true});
-    });
