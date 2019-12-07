480b4ae55eda1170076bbf06050973e2851a3f8e
Auggie <jhudak@alumni.berklee.edu>
2019-12-07 14:27:19 -0500

    refactor tests to use R.pathOr

--- test/module-1/module-1.test.js
+++ test/module-1/module-1.test.js
@@ -3,6 +3,8 @@ const proxyquire = require('proxyquire');
 const request = require('supertest');
 const jsdom = require('jsdom');
 const app = require('../../api/app');
+const assert = require('assert');
+const R = require('ramda');
 const {JSDOM} = jsdom;
 
 describe('module 1', () => {
@@ -32,79 +34,70 @@ describe('module 1', () => {
       });
 
       it('has a post method @set-the-encoding', () => {
-        try {
-          expect(form.attributes['method'].value).to.equal('post');
-        } catch (err) {
-          throw new Error('the `form` does not have a `method` of `post`');
-        }
+        expect(
+            'post',
+            'the `form` does not have a `method` of `post`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'method', 'value'], form));
       });
 
       it('has an enctype attribute with a value of multipart/form-data @set-the-encoding', () => {
-        try {
-          expect(form.attributes['enctype'].value).to.equal('multipart/form-data');
-        } catch (err) {
-          throw new Error('the `form` does not have an `enctype` of `multipart/form-data`');
-        }
+        expect(
+            'multipart/form-data',
+            'the `form` does not have an `enctype` of `multipart/form-data`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'enctype', 'value'], form));
       });
 
       it('has an action attribute with a value of /upload @set-the-encoding', () => {
-        try {
-          expect(form.attributes['action'].value).to.equal('/upload');
-        } catch (err) {
-          throw new Error('the `form` does not have an `action` of `/upload`');
-        }
+        expect(
+            '/upload',
+            'the `form` does not have an `action` of `/upload`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'action', 'value'], form));
       });
     });
 
     context('File Input', () => {
       it('has an input tag with a class of file-input @add-an-input', () => {
-        try {
-          expect(fileInput.className).to.equal('file-input');
-        } catch (err) {
-          throw new Error('the `form` does not contain an `input` with a `class` name  of `file-input`');
-        }
+        expect(
+            'file-input',
+            'the `form` does not contain an `input` with a `class` name  of `file-input`'
+        ).to.equal(R.pathOr(undefined, ['className'], fileInput));
       });
 
       it('has a type attribute with a value of file @add-an-input', () => {
-        try {
-          expect(fileInput.attributes['type'].value).to.equal('file');
-        } catch (err) {
-          throw new Error('the `input` does not have a `type` with a `value` of `file`');
-        }
+        expect(
+            'file',
+            'the `input` does not have a `type` with a value of `file`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'type', 'value'], fileInput));
       });
 
       it('has a name attribute with a value of photo @add-an-input', () => {
-        try {
-          expect(fileInput.attributes['name'].value).to.equal('photo');
-        } catch (err) {
-          throw new Error('the `input` does not have a `name` with a `value` of `photo`');
-        }
+        expect(
+            'photo',
+            'the `input` does not have a `name` with a `value` of `photo`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'name', 'value'], fileInput));
       });
     });
 
     context('Submit Button', () => {
       it('has an input tag with a class of submit-button @add-a-submit-button', () => {
-        try {
-          expect(submitButton.className).to.equal('submit-button');
-        } catch (err) {
-          throw new Error('the `form` does not contain an `input` tag with a `class` name of `submit-button`');
-        }
+        expect(
+            'submit-button',
+            'the `form` does not contain an `input` tag with a `class` name of `submit-button`'
+        ).to.equal(R.pathOr(undefined, ['className'], submitButton));
       });
 
       it('has a type attribute with a value of submit @add-a-submit-button', () => {
-        try {
-          expect(submitButton.attributes['type'].value).to.equal('submit');
-        } catch (err) {
-          throw new Error('the `submit-button` does not have a `type` with a value of `submit`');
-        }
+        expect(
+            'submit',
+            'the `submit-button` does not have a `type` with a value of `submit`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'type', 'value'], submitButton));
       });
 
       it('has a value attribute with a value of Submit @add-a-submit-button', () => {
-        try {
-          expect(submitButton.attributes['value'].value).to.equal('Submit');
-        } catch (err) {
-          throw new Error('the `submit-button` does not have a `value` attribute with a value of `Submit`');
-        }
+        expect(
+            'Submit',
+            'the `submit-button` does not have a `value` attribute with a value of `Submit`'
+        ).to.equal(R.pathOr(undefined, ['attributes', 'value', 'value'], submitButton));
       });
     });
   });
@@ -131,38 +124,26 @@ describe('module 1', () => {
       sinon.restore();
     });
 
-    it('should save the results of calling path.join to a constant @respond-with-the-form', () => {
-      const expectedDirname = path.resolve(__dirname, '../../api');
+    it('should save the results of calling path.resolve to a constant @respond-with-the-form', () => {
+      expect(
+          path.resolve(__dirname, '../../api'),
+          'Did you pass `__dirname` as the first argument to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['firstCall', 'args', 0], pathResolveStub));
 
-      try {
-        expect(expectedDirname).to.equal(pathResolveStub.firstCall.args[0]);
-      } catch (err) {
-        throw new Error('Did you pass `__dirname` as the first argument to `path.resolve()`?');
-      }
-
-      try {
-        expect('../client/index.html').to.equal(pathResolveStub.firstCall.args[1]);
-      } catch (err) {
-        throw new Error('Did you pass `\'../client/index.html\'` as the second argument to `path.resolve()`?');
-      }
+      expect(
+          '../client/index.html',
+          'Did you pass `\'../client/index.html\'` as the second argument to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['firstCall', 'args', 1], pathResolveStub));
     });
 
-    it('app.use should be called with /* as it\'s route', () => {
-      try {
-        expect(useSpy.firstCall.args[0]).to.equal('/*');
-      } catch (err) {
-        throw new Error('Did you pass `/*` as the first argument to `app.use()`?');
-      }
+    it('app.use should be called with /* as it\'s route @respond-with-the-form', () => {
+      expect(useSpy.calledWith('/*'), 'Did you pass `/*` as the first argument to `app.use()`?').to.be.true;
     });
   });
 
   context('app.js integration tests', () => {
     it('instantiates an express app @export-the-app', () => {
-      try {
-        expect(typeof app).to.be.equal('function');
-      } catch (err) {
-        throw new Error('did you remember to export the app?');
-      }
+      expect(typeof app, 'did you remember to export the app?').to.equal('function');
     });
 
     it('should serve our html file @respond-with-the-form', async () => {
@@ -172,7 +153,7 @@ describe('module 1', () => {
             .expect(200)
             .expect('Content-Type', /html/);
       } catch (err) {
-        throw new Error('did you call `response.sendFile()` with the correct argument inside the callback?');
+        throw new assert.AssertionError({message: 'did you call `response.sendFile()` with the correct argument inside the callback?'});
       }
     });
   });
