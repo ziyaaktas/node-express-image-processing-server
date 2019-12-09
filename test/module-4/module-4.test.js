23b422fbc5da180339e30d8217d5c753e48a602c
Auggie <jhudak@alumni.berklee.edu>
2019-12-08 22:05:59 -0500

    adds tags and error messages

--- test/module-4/module-4.test.js
+++ test/module-4/module-4.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const proxyquire = require('proxyquire').noCallThru();
+const R = require('ramda');
 const request = require('supertest');
 const rewire = require('rewire');
 const {JSDOM} = require('jsdom');
@@ -10,21 +11,32 @@ const app = require('../../api/app');
 
 describe('module 4', () => {
   context('imageProcessor.js', () => {
-    it('You should export the image processor function', () => {
-      expect(typeof imageProcessor).to.equal('function');
+    it('You should export the image processor function @create-the-image-processor', () => {
+      expect(typeof imageProcessor, 'Did you remember to export the `imageProcessor()`?').to.equal('function');
     });
 
-    it('the image processor function should return a promise', async () => {
-      const x = proxyquire('../../api/src/imageProcessor', {
-        worker_threads: {
-          isMainThread: false,
-        },
-      });
+    it('the image processor function should return a promise @instantiate-a-promise', async () => {
+      try {
+        const x = proxyquire('../../api/src/imageProcessor', {
+          worker_threads: {
+            isMainThread: false,
+          },
+        });
 
-      await assert.rejects(x('fsaf'), {message: 'not on main thread'});
+        await assert.rejects(
+            x('fsaf'),
+            new Error('not on main thread'),
+            'Did you reject the promise if it\'s not on the main thread? Did you pass the correct message?'
+        );
+      } catch (err) {
+        if (err.code === 'ERR_ASSERTION') {
+          throw err;
+        }
+        throw new Error('Did you return a promise from the `imageProcessor()`?');
+      }
     });
 
-    it('should assign a call to path.resolve to the constant pathToResizeWorker', () => {
+    it('should assign a call to path.resolve to the constant pathToResizeWorker @resolve-the-resize-worker', () => {
       const pathStub = sinon.stub();
       proxyquire('../../api/src/imageProcessor', {
         path: {
@@ -32,11 +44,18 @@ describe('module 4', () => {
         },
       });
 
-      expect(pathStub.firstCall.args[0]).to.equal(path.resolve(__dirname + '../../../api/src'));
-      expect(pathStub.firstCall.args[1]).to.equal('resizeWorker.js');
+      expect(
+          path.resolve(__dirname + '../../../api/src'),
+          'Did you pass `__dirname` to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['firstCall', 'args', 0], pathStub));
+
+      expect(
+          'resizeWorker.js',
+          'Did you pass `\'resizeWorker.js\'` to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['firstCall', 'args', 1], pathStub));
     });
 
-    it('should assign a call to path.resolve to the constant pathToMonochromeWorker', () => {
+    it('should assign a call to path.resolve to the constant pathToMonochromeWorker @resolve-the-monochrome-worker', () => {
       const pathStub = sinon.stub();
       proxyquire('../../api/src/imageProcessor', {
         path: {
@@ -44,18 +63,33 @@ describe('module 4', () => {
         },
       });
 
-      expect(pathStub.secondCall.args[0]).to.equal(path.resolve(__dirname + '../../../api/src'));
-      expect(pathStub.secondCall.args[1]).to.equal('monochromeWorker.js');
+      expect(
+          path.resolve(__dirname + '../../../api/src'),
+          'Did you pass `__dirname` to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['secondCall', 'args', 0], pathStub));
+
+      expect(
+          'monochromeWorker.js',
+          'Did you pass `\'monochromeWorker.js\'` to `path.resolve()`?'
+      ).to.equal(R.pathOr(undefined, ['secondCall', 'args', 1], pathStub));
     });
 
-    it('should define a function uploadPathResolver that takes a filename as it\s argument', () => {
-      const uploadPathResolver = rewire('../../api/src/imageProcessor').__get__('uploadPathResolver');
+    it('should define a function uploadPathResolver that takes a filename as it\s argument @create-upload-path-resolver', () => {
+      let uploadPathResolver;
+      try {
+        uploadPathResolver = rewire('../../api/src/imageProcessor').__get__('uploadPathResolver');
+      } catch (err) {
+        throw new Error('Did you remember to define the `uploadPathResolver()`?');
+      }
       const expected = uploadPathResolver('kittens.png');
 
-      expect(path.resolve(__dirname + '../../../api/uploads/kittens.png')).to.equal(expected);
+      expect(
+          path.resolve(__dirname + '../../../api/uploads/kittens.png'),
+          'Did you return a call to `path.resolve()` with the correct arguments?'
+      ).to.equal(expected);
     });
 
-    it('should assign a call to uploadPathResolver to a constant', async () => {
+    it('should assign a call to uploadPathResolver to a constant @define-the-source-path', async () => {
       const pathStub = sinon.stub();
       const imageProcessorProxy = proxyquire('../../api/src/imageProcessor', {
         'path': {
@@ -74,10 +108,13 @@ describe('module 4', () => {
         // do nothing
       }
 
-      expect(pathStub.thirdCall.args[2]).to.equal('moarKittens.png');
+      expect(
+          'moarKittens.png',
+          'Did you pass in the `filename` argument to `uploadPathResolver()`?',
+      ).to.equal(R.pathOr(undefined, ['thirdCall', 'args', 2], pathStub));
     });
 
-    it('should assign a call to uploadPathResolver to a constant passing in resized- concatenated with the filename', async () => {
+    it('should assign a call to uploadPathResolver to a constant passing in resized- concatenated with the filename @define-resized-destination', async () => {
       const pathStub = sinon.stub();
       const imageProcessorProxy = proxyquire('../../api/src/imageProcessor', {
         'path': {
@@ -96,10 +133,14 @@ describe('module 4', () => {
         // do nothing
       }
 
-      expect(pathStub.getCall(3).args[2]).to.equal('resized-moarKittens.png');
+      const pathStubCall = pathStub.getCall(3);
+      expect(
+          'resized-moarKittens.png',
+          'Did you concatenate `\'resized-\'` with `filename`?',
+      ).to.equal(R.pathOr(undefined, ['args', 2], pathStubCall));
     });
 
-    it('should assign a call to uploadPathResolver to a constant passing in monochrome- concatenated to the filename', async () => {
+    it('should assign a call to uploadPathResolver to a constant passing in monochrome- concatenated to the filename @define-monochrome-destination', async () => {
       const pathStub = sinon.stub();
       const imageProcessorProxy = proxyquire('../../api/src/imageProcessor', {
         'path': {
@@ -118,10 +159,14 @@ describe('module 4', () => {
         // do nothing
       }
 
-      expect(pathStub.getCall(4).args[2]).to.equal('monochrome-moarKittens.png');
+      const pathStubCall = pathStub.getCall(4);
+      expect(
+          'monochrome-moarKittens.png',
+          'Did you concatenate `\'monochrome-\'` with `filename`?',
+      ).to.equal(R.pathOr(undefined, ['args', 2], pathStubCall));
     });
 
-    it('should assign an instantiation of the Worker class to a constant representing the resizeWorker.', async () => {
+    it('should assign an instantiation of the Worker class to a constant representing the resizeWorker @instantiate-the-resize-worker', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/resizeStub.js'));
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/resizeStub.js'));
@@ -149,10 +194,15 @@ describe('module 4', () => {
             destination: path.resolve(__dirname, '../../api/uploads/resized-hereIsTheFile.png'),
           },
         }];
-      expect(workerStub.getCall(0).args).to.eql(expected);
+
+      const workerStubCall = workerStub.getCall(0);
+      expect(
+          expected,
+          'Did you pass `pathToResizeWorker` and the config object to a new instantiation of the `Worker()` class?'
+      ).to.eql(R.pathOr(undefined, ['args'], workerStubCall));
     });
 
-    it('should assign an instantiation of the Worker class to a constant representing the monochromeWorker.', async () => {
+    it('should assign an instantiation of the Worker class to a constant representing the monochromeWorker @instantiate-the-monochrome-worker', async () => {
       const workerStub = sinon.stub();
       const imageProcessorProxy = proxyquire('../../api/src/imageProcessor', {
         worker_threads: {
@@ -175,10 +225,15 @@ describe('module 4', () => {
             destination: path.resolve(__dirname, '../../api/uploads/monochrome-hereIsTheFile.png'),
           },
         }];
-      expect(workerStub.getCall(1).args).to.eql(expected);
+
+      const workerStubCall = workerStub.getCall(1);
+      expect(
+          expected,
+          'Did you pass `pathToMonochromeWorker` and the config object to a new instantiation of the `Worker()` class?'
+      ).to.eql(R.pathOr(undefined, ['args'], workerStubCall));
     });
 
-    it('should resolve the promise on resizeWorker\'s \'message\' event', async () => {
+    it('should resolve the promise on resizeWorker\'s \'message\' event @resize-on-message', async () => {
       let result;
 
       const imageProcessor = rewire('../../api/src/imageProcessor');
@@ -191,35 +246,53 @@ describe('module 4', () => {
         // do nothing
       }
 
-
-      return expect(result).to.equal('resizeWorker finished processing');
+      return expect(
+          result,
+          'Did you register an `on(\'message\')` event listener for the resizeWorker that resolves the promise?'
+      ).to.equal('resizeWorker finished processing');
     });
 
-    it('should reject the promise on resizeWorker\'s \'error\' event', async () => {
+    it('should reject the promise on resizeWorker\'s \'error\' event @resize-error-event', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
+      let errorThrown;
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/resizeWorkerProxyError.js'));
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/monochromeStub.js'));
 
       try {
         await imageProcessor('ullr.png');
       } catch (err) {
-        expect('Error from worker thread').to.eql(err.message);
+        errorThrown = true;
+        expect(
+            'Error from worker thread',
+            'Did you register an `on(\'error\')` event listener for the resizeWorker that rejects the promise?'
+        ).to.eql(err.message);
+      } finally {
+        if (!errorThrown) {
+          throw new Error('Did you register an `on(\'error\')` event listener for the resizeWorker that rejects the promise?');
+        }
       }
     });
 
-    it('should reject the promise on resizeWorker\'s \'exit\' event when process exit\'s > 0', async () => {
+    it('should reject the promise on resizeWorker\'s \'exit\' event when process exit\'s  > 0 @resize-exit-event', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
+      setTimeout(() => {
+        throw new Error('Did you register an `on(\'exit\')` event listener for the resizeWorker that rejects the promise?');
+      }, 2000);
+
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/resizeWorkerProxyExitCode1.js'));
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/monochromeStub.js'));
 
       try {
         await imageProcessor('ullr.png');
       } catch (err) {
-        expect('Exited with status code 1').to.eql(err.message);
+        expect(
+            'Exited with status code 1',
+            'Did you register an `on(\'exit\')` event listener for the resizeWorker that rejects the promise?'
+        ).to.eql(err.message);
       }
     });
 
-    it('should resolve the promise on monochromeWorker\'s \'message\' event', async () => {
+    it('should resolve the promise on monochromeWorker\'s \'message\' event @monochrome-message-event', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/resizeWorkerProxy.js'));
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/monochromeFast.js'));
@@ -230,32 +303,46 @@ describe('module 4', () => {
         // do nothing
       }
 
-      return expect(result).to.equal('monochromeWorker finished processing');
+      return expect(
+          result,
+          'Did you register an `on(\'message\')` event listener for the monochromeWorker that resolves the promise?'
+      ).to.equal('monochromeWorker finished processing');
     });
 
-    it('should reject the promise on monochromeWorker\'s \'error\' event', async () => {
+    it('should reject the promise on monochromeWorker\'s \'error\' event @monochrome-error-event', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
+      let errorThrown;
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/resizeWorkerProxyError.js'));
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/monochromeStub.js'));
 
       try {
         await imageProcessor('ullr.png');
       } catch (err) {
-        expect('Error from worker thread').to.eql(err.message);
+        errorThrown = true;
+        expect(
+            'Error from worker thread',
+            'Did you register an `on(\'error\')` event listener for the monochromeWorker that rejects the promise?'
+        ).to.eql(err.message);
         // do nothing
+      } finally {
+        if (!errorThrown) {
+          throw new Error('Did you register an `on(\'error\')` event listener for the resizeWorker that rejects the promise?');
+        }
       }
     });
 
-    it('should reject the promise on monochromeWorker\'s \'exit\' event when process exit\'s > 0', async () => {
+    it('should reject the promise on monochromeWorker\'s \'exit\' event when process exit\'s > 0 @monochrome-exit-event', async () => {
       const imageProcessor = rewire('../../api/src/imageProcessor');
+      setTimeout(() => {
+        throw new Error('Did you register an `on(\'exit\')` event listener for the monochromeWorker that rejects the promise?');
+      }, 2000);
       imageProcessor.__set__('pathToMonochromeWorker', path.resolve('./test/module-4/resizeWorkerProxyExitCode1.js'));
       imageProcessor.__set__('pathToResizeWorker', path.resolve('./test/module-4/monochromeStub.js'));
 
       try {
         await imageProcessor('ullr.png');
       } catch (err) {
-        expect('Exited with status code 1').to.eql(err.message);
-        // do nothing
+        expect('Exited with status code 1', 'Did you register an `on(\'exit\')` event listener for the monochromeWorker that rejects the promise?').to.eql(err.message);
       }
     });
   });
@@ -263,7 +350,7 @@ describe('module 4', () => {
   context('router.js', async () => {
     const directory = path.resolve(__dirname + '../../../api/uploads');
 
-    it('should add a call to imageProcessor passing in the image in our post request', async () => {
+    it('should add a call to imageProcessor passing in the image in our post request @invoke-the-image-processor', async () => {
       let files = fs.readdirSync(directory);
 
       for (const file of files) {
@@ -278,9 +365,9 @@ describe('module 4', () => {
 
       files = fs.readdirSync(directory);
 
-      expect(files.includes('ullr.png')).to.be.true;
-      expect(files.includes('resized-ullr.png')).to.be.true;
-      expect(files.includes('monochrome-ullr.png')).to.be.true;
+      expect(files.includes('ullr.png'), 'Did you call `imageProcessor()` inside the `\'/upload\'` route?').to.be.true;
+      expect(files.includes('resized-ullr.png'), 'Did you call `imageProcessor()` inside the `\'/upload\'` route?').to.be.true;
+      expect(files.includes('monochrome-ullr.png'), 'Did you call `imageProcessor()` inside the `\'/upload\'` route?').to.be.true;
     });
   });
 
@@ -298,12 +385,18 @@ describe('module 4', () => {
       sinon.restore();
     });
 
-    it('should add an img tag with an src of monochrome-ullr.png', () => {
-      expect(images[1].attributes.src.value).to.equal('monochrome-ullr.png');
+    it('should add an img tag with an src of monochrome-ullr.png @add-monochrome-image', () => {
+      expect(
+          'monochrome-ullr.png',
+          'Did you add an `<img>` with an `src` of `\'monochrome-ullr.png\'` to the photo-viewer?'
+      ).to.equal(R.pathOr(undefined, [1, 'attributes', 'src', 'value'], images));
     });
 
-    it('should add an img tag with an src of resized-ullr.png', () => {
-      expect(images[2].attributes.src.value).to.equal('resized-ullr.png');
+    it('should add an img tag with an src of resized-ullr.png @add-resized-image', () => {
+      expect(
+          'resized-ullr.png',
+          'Did you add an `<img>` with an `src` of `\'resized-ullr.png\'` to the photo-viewer?'
+      ).to.equal(R.pathOr(undefined, [2, 'attributes', 'src', 'value'], images));
     });
   });
 });
