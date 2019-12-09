1f8d5f13cd514b6817dae8ca52fe365d7ec98598
Auggie <jhudak@alumni.berklee.edu>
2019-12-09 16:09:26 -0500

    remove chai-as-promised from test runner

--- test/mocha-config.js
+++ test/mocha-config.js
@@ -1,13 +1,11 @@
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
-chai.use(sinonChai)
-
+chai.use(sinonChai);
 
 
 Object.assign(global, {
   expect: chai.expect,
-  sinon
-})
+  sinon,
+});
